80f69688433475097ce024a08d7a4c8d2c6d045d
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-05 11:35:22 +0300

    add example ternar operator

---
+++ sandbox.php
@@ -0,0 +1,28 @@
+<?php
+
+$score = 50;
+
+// $val = $score > 40 ? 'high score!' : 'low score :(';
+// echo $val;
+
+// echo $score > 40 ? 'high score!' : 'low score :(';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-MySQL Tutorals - Sandbox</title>
+</head>
+
+<body>
+
+    <p><?php echo $score > 40 ? 'high score!' : 'low score :('; ?></p>
+
+</body>
+
+</html>
