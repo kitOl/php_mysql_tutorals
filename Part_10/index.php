f95c1d4e539dc4da446d01406b274337809da7bd
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-05 09:11:19 +0300

    final project pizza

--- Part_10/index.php
+++ Part_10/index.php
@@ -13,6 +13,9 @@ mysqli_close($conn);
 
 <!DOCTYPE html>
 <html lang="en">
+  <style>
+
+  </style>
 
 <?php include('templates/header.php'); ?>
 
@@ -25,6 +28,7 @@ mysqli_close($conn);
 
       <div class="col s6 md3">
         <div class="card z-depth-0">
+          <img src="img/pizza.svg" alt="Pizza" class="pizza">
           <div class="card-content center">
             <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
             <ul>
