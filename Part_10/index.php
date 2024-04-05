cd67b25ff93c3983609909b0b658d81433754945
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-05 04:15:42 +0300

    add pizza cards on main page

--- Part_10/index.php
+++ Part_10/index.php
@@ -7,10 +7,10 @@ $db_name = 'ninja_pizza';
 $conn = mysqli_connect($host, $username, $password, $db_name);
 
 if (!$conn) {
-    echo "Connection error: " . mysqli_connect_errno();
+  echo "Connection error: " . mysqli_connect_errno();
 }
 
-$sql = "SELECT title, ingredients, id FROM pizzas";
+$sql = "SELECT title, ingredients, id FROM pizzas ORDER BY created_at";
 $result = mysqli_query($conn, $sql);
 $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
@@ -25,6 +25,30 @@ print_r($pizzas);
 <html lang="en">
 
 <?php include('templates/header.php'); ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+  <div class="row">
+
+    <?php foreach ($pizzas as $pizza) { ?>
+
+      <div class="col s6 md3">
+        <div class="card z-depth-0">
+          <div class="card-content center">
+            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+            <div class="card-action right-align">
+              <a href="#" class="brand-text">more info</a>
+            </div>
+          </div>
+        </div>
+
+      <?php } ?>
+      
+      </div>
+  </div>
+</div>
 
 <?php include('templates/footer.php'); ?>
 
