e96782b0cd86436a2e9bf04c3c4b498907e9a95f
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-05 04:00:44 +0300

    create db ninja_pizza & table pizzas & connect to

--- Part_10/add.php
+++ Part_10/add.php
@@ -47,7 +47,6 @@ if (isset($_POST['submit'])) {
     }
 }
 
-
 ?>
 
 <!DOCTYPE html>
--- Part_10/index.php
+++ Part_10/index.php
@@ -1,5 +1,23 @@
 <?php
+$host = 'localhost';
+$username = 'ninja';
+$password = '123456';
+$db_name = 'ninja_pizza';
 
+$conn = mysqli_connect($host, $username, $password, $db_name);
+
+if (!$conn) {
+    echo "Connection error: " . mysqli_connect_errno();
+}
+
+$sql = "SELECT title, ingredients, id FROM pizzas";
+$result = mysqli_query($conn, $sql);
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+mysqli_close($conn);
+
+print_r($pizzas);
 
 ?>
 
